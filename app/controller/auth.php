a21e18c3d312640b3582a400061cb6fe167c8e4a
Marco Dickert <marco@misterunknown.de>
2017-01-17 21:42:29 +0100

    auth controller fixed

--- app/controller/auth.php
+++ app/controller/auth.php
@@ -22,16 +22,9 @@ class Auth extends Base {
         if( $f3->exists( 'POST.login' ) && $f3->exists( 'POST.password' ) ) {
             sleep( 3 ); // login should take a while to kick-ass brute force attacks
             $user = new \Model\User();
-            $user->load( array( 'login = ?', $f3->get( 'POST.username' ) ) );
+            $user->load( array( 'login = ?', $f3->get( 'POST.login' ) ) );
             if( ! $user->dry() ) {
-                // check hash engine
-                $valid = false;
-                if( $hash_engine == 'bcrypt' ) {
-                    $valid = \Bcrypt::instance()->verify( $f3->get( 'POST.password' ), $user->password );
-                } elseif( $hash_engine == 'md5' ) {
-                    $valid = ( md5( $f3->get( 'POST.password' ) . $f3->get( 'password_md5_salt' ) ) == $user->password );
-                }
-                if( $valid ) {
+                if( $user->password == password_hash( $f3->get( 'POST.password' ), PASSWORD_BCRYPT, array( "salt" => $f3->get( 'global_salt' ) ) ) ) {
                     @$f3->clear( 'SESSION' ); //recreate session id
                     $f3->set( 'SESSION.user_id', $user->id );
                     $f3->reroute('/dashboard');
