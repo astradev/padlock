9f56bfa4634a437dd9995b6449ebe2e5c2fe1735
Marco Dickert <marco@misterunknown.de>
2017-01-18 15:53:21 +0100

    dashboard fix

--- app/controller/dashboard.php
+++ app/controller/dashboard.php
@@ -4,7 +4,7 @@ namespace Controller;
 
 class Dashboard extends Base {
 	public function show( \Base $f3, $params ) {
-		$f3->set( 'folders'  = \Permissions::instance()->getFolders( $f3->get( 'SESSION.user.id' ) );
+		$f3->set( 'folders', \Permissions::instance()->getFolders( $f3->get( 'SESSION.user.id' ) ) );
 		// template information
 		$f3->set( 'content', 'dashboard.html' );
 	}
