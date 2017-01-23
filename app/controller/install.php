57c511036bcd76a126b9576ed79d3efea7bf145d
astradev <leo@astradev.de>
2017-01-23 16:22:19 +0100

    started with install setup

--- app/controller/install.php
+++ app/controller/install.php
@@ -9,11 +9,9 @@ class Installer {
         }
 
         public function createConfig( $f3, $params ) {
-            //config mit startparametern + zusatzparameter (vom User) generieren 
         }
 
         public function createAccount( $f3, $params ) {
-          //account createn mit eingegebenen werten vom User
           if( $f3->exsists( 'POST.login' ) && ctype_alpha( 'POST.name' ) ) {
             
           } else {
@@ -22,29 +20,22 @@ class Installer {
         }
 
         public function selectDb( $f3, $params ) {
-            //wählen (MySQL, MongoDb, PostgreSQL, ...)
         }
 
         public function createDb( $f3, $params ) {
-            //eingegebenen Werte generieren (Db name, vorgegebene Werte, ....)
         }
 
         public function injectDb( $f3, $params ) {
-            //db in dbserver injecten
         }
 
         public function finish( $f3, $params ) {
-          //check for submit
           if( $f3->exists( 'POST.submit' ) ) {
-              //delete install files
               unlink( '../templates/install.html' );
               unlink( 'install.php' );
 
-              //set message and go to dashboard
               $f3->SESSION->messages[] = array( "Install successfully finished", 0 );
               $f3->reroute( '/dashboard' );
           } else {
-              //error message + reroute
               $f3->SESSION->messages[] = array( "An error occurred during the installation", 1 );
               $f3->reroute( '/install' );
         }
