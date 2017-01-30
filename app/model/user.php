4d6915dbda869e2e79cf1b2c548489556b6c39dc
astradev <leo@astradev.de>
2017-01-30 07:28:51 +0100

    added select list 'admin rights'

--- app/model/user.php
+++ app/model/user.php
@@ -15,18 +15,4 @@ class User extends Base {
 	  return $this->find();
   }
 
-  public function getSuperuser() {
-	$f3 = \BASE::instance();
-	$list = $f3->DB->exec( "SELECT * FROM users WHERE id = 2" );
-
-	while( !empty( $list ) ) {
-		$currNode = array_shift( $list );
-                $result .= '<option value="'.$currNode['superuser'].'"';
-                if( $currNode['superuser'] == 1 ) { $result .= ' selected="selected"'; } else { $result .= ' selected="selected"'; }
-		$result .= '>';
-		if( $currNode["superuser"] == 1 ) { $result .= 'Yes'; } else { $result .= 'No'; }
-		$result .= '</option>';
-	}
-	return $result;
-  }
 }
