5359975910dfe088ea2013301ab46dde0e1981b0
Marco Dickert <marco@misterunknown.de>
2017-02-08 14:00:26 +0100

    fixed permission form

--- app/controller/permissions.php
+++ app/controller/permissions.php
@@ -14,13 +14,11 @@ class Permissions extends Backend {
 //		if( ! in_array( $params['mode'], array( 'byrole', 'byfolder' ) ) ) {
 //			$f3->reroute( '/settings/permissions/byfolder' );
 //		}
-		$folder = new \Model\Folder();
 		$role = new \Model\Role();
 
 		$f3->set( 'permissions', \Permissions::instance()->getAllPermissions() );
-		$f3->set( 'folders', $folder->find() );
 		$f3->set( 'roles', $role->find() );
-		$f3->set( 'optionFolders', \TreeBuilder::instance()->generateOptionTree() );
+		$f3->set( 'optionFolders', \Treebuilder::instance()->generateOptionTree() );
 		$f3->set( 'section', 'permissions.html' );
 	}
 
